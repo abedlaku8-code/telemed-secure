4ff2e13f0ccb81368792d275c56c617a56d29e0f
Laku Ishako Abednego <Abedlaku8@gmail.com>
2026-09-19 11:08:44 +0100

    Configurer CORS pour le frontend Render

--- Api/main.py
+++ Api/main.py
@@ -76,7 +76,8 @@ application.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://telemed-secure-frontend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
